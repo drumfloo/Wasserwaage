5fd740fabf758a5ce76dcf4a82726fde0b76236d
drumfloo <drumflo@protonmail.com>
2024-02-02 22:08:13 +0100

    first try configpager layout

--- App_Python/main.py
+++ App_Python/main.py
@@ -1,19 +1,27 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.lang.builder import Builder
 
 
-class ScaleGridLayout(GridLayout):
-    pass
 
-class ConfigGridLayout(GridLayout):
-    pass
+class config_page(BoxLayout):
+    Builder.load_file('config_page.kv')
+    def test(self):
+        pass
+
+# class ConfigGridLayout(GridLayout):
+#     pass
 
 
 
 class ScaleApp(App):
     def build(self):
         self.title = 'Scale-App'
-        return ScaleApp()
+        return config_page()
 
 if __name__ == '__main__':
     ScaleApp().run()
+
+
+# ['lr-tb', 'tb-lr', 'rl-tb', 'tb-rl', 'lr-bt', 'bt-lr', 'rl-bt', 'bt-rl']
