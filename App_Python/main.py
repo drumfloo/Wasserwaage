6ba0f9bc77f6794e5ff3131a9ae5feb3ee9a65b9
drumfloo <drumflo@protonmail.com>
2024-02-02 14:35:41 +0100

    initial

--- App_Python/main.py
+++ App_Python/main.py
@@ -1,10 +1,18 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
-import ScaleApp
+
+
+class ScaleGridLayout(GridLayout):
+    pass
+
+class ConfigGridLayout(GridLayout):
+    pass
+
+
 
 class ScaleApp(App):
     def build(self):
-        self.title = 'Multimedia App'
+        self.title = 'Scale-App'
         return ScaleApp()
 
 if __name__ == '__main__':
