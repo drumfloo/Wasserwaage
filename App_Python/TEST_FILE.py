ecd0c106d738ef1a3673e22d85210d7899033e05
drumfloo <drumflo@protonmail.com>
2024-02-08 14:44:10 +0100

    integrated 2nd page

--- App_Python/TEST_FILE.py
+++ App_Python/TEST_FILE.py
@@ -1,97 +1,13 @@
-<config_page>:
-    orientation: 'vertical'
-    padding: 10
-    spacing: 10
+import android
+import time
 
-    BoxLayout:
-        orientation: 'horizontal'
-        size_hint_y: None
-        height: dp(50)
-
-        Label:
-            text: 'MQTT-Host'
-            size_hint_x: 0.2
-        TextInput:
-            id: mqtt_host_input
-            size_hint_x: 0.4
-
-        Label:
-            text: 'Port'
-            size_hint_x: 0.1
-        TextInput:
-            id: port_input
-            size_hint_x: 0.3
-        
-    BoxLayout:
-        orientation: 'horizontal'
-        size_hint_y: None
-        height: dp(50)
-
-        Label:
-            text: "User Name"
-            size_hint_x: 0.2
-        TextInput:
-            id: username_input
-            size_hint_x: 0.4
-
-        Label:
-            text: 'Password'
-            size_hint_x: 0.1
-        TextInput:
-            id: password_input
-            size_hint_x: 0.3
-        
-    BoxLayout:
-        orientation: 'horizontal'
-        size_hint_y: None
-        height: dp(50)
-
-        Label:
-            text: "Full Topic"
-            size_hint_x: 0.2
-        TextInput:
-            id: full_topic_input
-            size_hint_x: 0.8
-    
-    BoxLayout:
-        orientation: 'horizontal'
-        size_hint_y: None
-        height: dp(50)
-
-        Label:
-            text: 'Intervals'
-            size_hint_x: 0.2
-        Spinner:
-            id: intervals_spinner
-            text: "Select Interval"
-            values: ["2sec","5sec","10sec"]
-            size_hint_x: 0.5  # Breite des Spinners
-
-        Label:
-            text: 'Dimensions'
-            size_hint_x: 0.2
-        Spinner:
-            id: dimensions_spinner
-            text: "Select Dimension"
-            values: ['1D','2D','3D']
-            size_hint_x: 0.5  # Breite des Spinners
-
-    BoxLayout:
-        orientation: 'horizontal'
-        size_hint_y: None
-        height: dp(50)
-
-        Button:
-            text: "Check Connection"
-            on_press: root.check_connection()
-            size_hint_x: 0.3
-
-        Button:
-            text: 'Go'
-            on_press: root.go()
-            size_hint_x: 0.3
-
-        Button:
-            text: 'Back'
-            on_press: root.back()
-            size_hint_x: 0.4
+droid = android.Android()
+dt = 100 #100ms sensings
+endTime = 3000 #sample for 3000ms
+timeSensed=0
+droid.startSensingTimed(2,dt) 
+while timeSensed <= endTime:
+    print droid.sensorsReadAccelerometer().result
+    time.sleep(dt/1000.0)
+    timeSensed+=dt
+droid.stopSensing()
--- App_Python/main.py
+++ App_Python/main.py
@@ -1,36 +1,90 @@
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.boxlayout import BoxLayout
-from kivy.lang.builder import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
 
 from plyer import accelerometer
 
-class config_page(BoxLayout):
-    Builder.load_file('config_page.kv')
-    
-    # def __init__(self, **kwargs):
-    #     super(config_page, self).__init__(**kwargs)
-    #     self.size_hint = (1, 1)
+class StartScreen(Screen):
+    #Builder.load_file('StartScreen.kv')
+    pass
 
+class ConfigScreen(Screen):
+    #Builder.load_file('ConfigScreen.kv')
+    pass
+    def check_connection(self):
+        print("Checking connection...")
 
+    def go(self):
+        print("Going to the next page...")
 
+    def back(self):
+        print("Going back...")
 
 class ScaleApp(App):
     def build(self):
-        self.title = 'Scale-App'
-        #self.get_acceleration()
-        return config_page()
-    
-    def get_acceleration(self):
-        accelerometer.enable()
-        acceleration = accelerometer.acceleration
-        #self.label.text = f"Neigung: {acceleration}"
-        print(acceleration)
+        self.title = 'Scale-App'        
+        #Builder.load_string(open('ConfigScreen.kv', encoding='utf-8').read())
+        #Builder.load_string(open('StartScreen.kv', encoding='utf-8').read())       
+        Builder.load_file('ConfigScreen.kv')
+        Builder.load_file('StartScreen.kv')
+        sm = ScreenManager()
+        start_screen = StartScreen(name='start')
+        config_screen = ConfigScreen(name='config')
+        sm.add_widget(start_screen)
+        sm.add_widget(config_screen)
+        return sm 
 
-
-
-
-
+    # def get_acceleration(self):
+    #     accelerometer.enable()
+    #     acceleration = accelerometer.acceleration
+    #     print(acceleration)
 
 if __name__ == '__main__':
     ScaleApp().run()
+
+
+
+
+
+
+
+
+
+# ERSTE FASSUNG CONFIG_SCREEN
+
+# from kivy.app import App
+# from kivy.uix.gridlayout import GridLayout
+# from kivy.uix.boxlayout import BoxLayout
+# from kivy.lang.builder import Builder
+
+# from plyer import accelerometer
+# from kivy.uix.screenmanager import ScreenManager, Screen
+
+# class Config_page(BoxLayout):
+#     Builder.load_file('Config_page.kv')
+    
+#     # def __init__(self, **kwargs):
+#     #     super(Config_page, self).__init__(**kwargs)
+#     #     self.size_hint = (1, 1)
+
+# class Scale_page(BoxLayout):
+#     pass
+
+
+
+# class ScaleApp(App):
+#     def build(self):
+#         self.title = 'Scale-App'
+#         #self.get_acceleration()
+#         return Config_page()
+    
+#     def get_acceleration(self):
+#         accelerometer.enable()
+#         acceleration = accelerometer.acceleration
+#         #self.label.text = f"Neigung: {acceleration}"
+#         print(acceleration)
+
+
+
+# if __name__ == '__main__':
+#     ScaleApp().run()
