d6d139f7543387df1eaa7b678f2240b69f6389dd
irockayII <grund.nik@outlook.de>
2024-03-01 14:48:51 +0100

    help

--- libelle.py
+++ libelle.py
@@ -16,18 +16,40 @@ APP_KV = """
             pos: (self.width-100)/2, (self.height-100)/2
 """
 
+
+#https://kivy.org/doc/stable/api-kivy.uix.floatlayout.html mal ansehen
+
 class CanvasTest(BoxLayout):    
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        Clock.schedule_interval(self.test_pos, 1)
+        self.pos_x = (self.width-100)/2
+        self.pos_y = (self.height-100)/2
+        Clock.schedule_interval(self.test_pos, 0.01)
         
-    def test_pos(self, *args):
-        print(self.height)
+    def test_pos(self, *args): 
+        pos_x_target_test = [700, 1]
+        pos_x_target = pos_x_target_test[0] if round(self.pos_x, 0) == round(pos_x_target_test[1], 0) else pos_x_target_test[1]
+        self.pos_y = (self.height-100)/2        
+        pos_x = self.get_x_pos(pos_x_target)
+        pos_y = self.pos_y
+        self.canvas.get_group('libelle')[0].pos = pos_x, pos_y
 
-        x_pos = (self.width-100)/2
-        y_pos = (self.height-100)/2
-        self.canvas.get_group('libelle')[0].pos = x_pos, y_pos
+
+    def get_x_pos(self, pos_x_target):       
+        self.pos_x = (self.width-100)/2 if self.pos_x < 1 else self.pos_x
+        if round(self.pos_x, 0) == round(pos_x_target, 0):    
+            print('equal') 
+            return self.pos_x
+        elif self.pos_x < pos_x_target: 
+            print('to right')           
+            # self.pos_x +=  (pos_x_target/self.pos_x)*2
+            self.pos_x += 10
+        elif self.pos_x > pos_x_target and self.pos_x > 0:    
+            print('to left')   
+            self.pos_x -= 10
+            # self.pos_x -=  ((self.width-pos_x_target)/(self.width-self.pos_x))*2
+        return self.pos_x
 
 
         # self.canvas.get_group('libelle')[0].pos = random.randrange(1, Window.width - 200), random.randrange(1, Window.height - 100)
