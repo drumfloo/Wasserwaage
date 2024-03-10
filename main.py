5b773e3ca9ee77e94ec3219282fcbe61fe905330
drumfloo <drumflo@protonmail.com>
2024-03-10 17:05:19 +0100

    Test of accelerator

--- main.py
+++ main.py
@@ -1,111 +1,55 @@
+'''
+Basic accelerometer example.
+'''
+
 from kivy.app import App
-from kivy.clock import Clock
-from kivy.lang import Builder
-from kivy.properties import NumericProperty
-from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
+from kivy.clock import Clock
 
-interface = Builder.load_string('''
-#:import facade plyer.gyroscope
-<GyroscopeInterface>:
-    facade: facade
-    orientation: 'vertical'
-    padding: '20dp'
-    spacing: '10dp'
-    BoxLayout:
-        orientation: 'vertical'
-        BoxLayout:
-            orientation: 'horizontal'
-            size_hint: 1, .1
-            Button:
-                id: enable_button
-                text: 'Enable Sensor'
-                disabled: False
-                on_release:
-                    root.enable()
-                    disable_button.disabled = not disable_button.disabled
-                    enable_button.disabled = not enable_button.disabled
-            Button:
-                id: disable_button
-                text: 'Disable Sensor'
-                disabled: True
-                on_release:
-                    root.disable()
-                    disable_button.disabled = not disable_button.disabled
-                    enable_button.disabled = not enable_button.disabled
-        BoxLayout:
-            orientation: 'vertical'
-            Label:
-                text: 'Rate of rotation'
-            Label:
-                text: 'including drift compensation'
-            Label:
-                text: '(' + str(root.x_calib) + ','
-            Label:
-                text: str(root.y_calib) + ','
-            Label:
-                text: str(root.z_calib) + ')'
-            Label:
-                text: 'Rate of rotation'
-            Label:
-                text: 'w/o drift compensation'
-            Label:
-                text: '(' + str(root.x_speed) + ','
-            Label:
-                text: str(root.y_speed) + ','
-            Label:
-                text: str(root.z_speed) + ')'
-            Label:
-                text: 'Estimated Drift'
-            Label:
-                text: '(' + str(root.x_drift) + ','
-            Label:
-                text: str(root.y_drift) + ','
-            Label:
-                text: str(root.z_drift) + ')'
-''')
+from plyer import accelerometer
 
 
-class GyroscopeInterface(BoxLayout):
+class AccelerometerTest(BoxLayout):
+    def __init__(self):
+        super().__init__()
+        self.sensorEnabled = False
 
-    x_calib = NumericProperty(0)
-    y_calib = NumericProperty(0)
-    z_calib = NumericProperty(0)
-    x_speed = NumericProperty(0)
-    y_speed = NumericProperty(0)
-    z_speed = NumericProperty(0)
-    x_drift = NumericProperty(0)
-    y_drift = NumericProperty(0)
-    z_drift = NumericProperty(0)
+    def do_toggle(self):
+        try:
+            if not self.sensorEnabled:
+                accelerometer.enable()
+                Clock.schedule_interval(self.get_acceleration, 1 / 20.)
 
-    facade = ObjectProperty()
+                self.sensorEnabled = True
+                self.ids.toggle_button.text = "Stop Accelerometer"
+            else:
+                accelerometer.disable()
+                Clock.unschedule(self.get_acceleration)
 
-    def enable(self):
-        self.facade.enable()
-        Clock.schedule_interval(self.get_rotation, 1 / 20.)
-        Clock.schedule_interval(self.get_rotation_uncalib, 1 / 20.)
+                self.sensorEnabled = False
+                self.ids.toggle_button.text = "Start Accelerometer"
+        except NotImplementedError:
+            import traceback
+            traceback.print_exc()
+            status = "Accelerometer is not implemented for your platform"
+            self.ids.accel_status.text = status
 
-    def disable(self):
-        self.facade.disable()
-        Clock.unschedule(self.get_rotation)
-        Clock.unschedule(self.get_rotation_uncalib)
+    def get_acceleration(self, dt):
+        val = accelerometer.acceleration[:3]
 
-    def get_rotation(self, dt):
-        if self.facade.rotation != (None, None, None):
-            self.x_calib, self.y_calib, self.z_calib = self.facade.rotation
-
-    def get_rotation_uncalib(self, dt):
-        empty = tuple([None for i in range(6)])
-
-        if self.facade.rotation_uncalib != empty:
-            self.x_speed, self.y_speed, self.z_speed, self.x_drift, \
-                self.y_drift, self.z_drift = self.facade.rotation_uncalib
+        if not val == (None, None, None):
+            self.ids.x_label.text = "X: " + str(val[0])
+            self.ids.y_label.text = "Y: " + str(val[1])
+            self.ids.z_label.text = "Z: " + str(val[2])
 
 
-class GyroscopeTestApp(App):
+class AccelerometerTestApp(App):
     def build(self):
-        return GyroscopeInterface()
+        return AccelerometerTest()
+
+    def on_pause(self):
+        return True
 
 
-if __name__ == "__main__":
-    GyroscopeTestApp().run()
+if __name__ == '__main__':
+    AccelerometerTestApp().run()
